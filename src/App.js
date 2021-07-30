ff67e06aa648a0b9981088a1e41bcf87b1290790
AkshayGupt <189301019.akshay@gmail.com>
2021-07-30 12:44:20 +0530

    :sparkles: Added Intro slides"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import Slides from './components/Introduction/Slides'
+import Slides from './components/Introduction/Slides';
+import Landing from './components/Landing/Landing'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
+
     <div className="App">
-        <Slides/>
+       <Router>
+        <Switch>
+          {/* Public Routes */}
+          <Route path="/" exact component={Slides} />
+          <Route path="/landing" exact component={Landing} />
+        </Switch>
+      </Router>
     </div>
   );
 }
