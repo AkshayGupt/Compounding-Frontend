1c00cfadde150e819b93dfccd3a651cd96763814
AkshayGupt <189301019.akshay@gmail.com>
2021-08-01 15:04:35 +0530

    :art: Select monster for transaction

--- src/components/Transactions/TransactionView.js
+++ src/components/Transactions/TransactionView.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
+import Monsters from "./Monsters";
 import './styles.css';
+import icon1 from '../../monster_icons/1.png';
+import icon2 from '../../monster_icons/2.png';
+import icon3 from '../../monster_icons/3.png';
+import icon4 from '../../monster_icons/4.png';
+
 const TransactionView = ({
   id = "",
   name = "Shopping",
@@ -9,6 +15,35 @@ const TransactionView = ({
   payment="Amazon",
   isMonster=false
 }) => {
+
+  
+
+  const monsters = [
+    {
+      icon:icon1,
+      id:0
+    },
+    {
+      icon:icon2,
+      id:1
+    },
+    {
+      icon:icon3,
+      id:2
+    },
+    {
+      icon:icon4,
+      id:3
+    },
+
+  ]
+
+  const [monster,setMonster] = useState();
+  // const [viewMonsterInMobile,setViewMonsterInMobile] = useState(false);
+
+  const selectMonster = (id) =>{
+    setMonster(id);
+  }
 
   const showForm = () =>{
     return(
@@ -66,32 +101,44 @@ const TransactionView = ({
         </textarea>
       </div>
       <div className="text-center">
-          <p className="btn btn-sm btn-primary mx-1"> <span class="btn-text">monster</span> <i class="fab fa-optin-monster"></i></p>
+          {/* <p onClick={()=>setViewMonsterInMobile(viewMonsterInMobile=> {return !viewMonsterInMobile})} className="btn btn-mobile btn-sm btn-primary mx-1"> <span class="btn-text">select monster</span> <i class="fab fa-optin-monster"></i></p> */}
           <p className="btn btn-sm btn-success mx-1"> <span class="btn-text">Update</span> <i class="fas fa-pencil-alt"></i></p>
           <p className="btn btn-sm btn-danger mx-1"> <span class="btn-text">Delete</span> <i class="fas fa-trash"></i></p>
       </div>
     </form>
     )
   }
+
+  const showMonster = () =>{
+    if(monster>=0){
+      return <img src={monsters[monster].icon} alt="monster" height="50px" width="auto"/>
+    }
+  }
+
   return (
     <div className="container">
         <div className="row">
-              <div className="col-3">
-                <Monsters/>
-              </div>
-              <div className="col-9">
+              <div className="col-md-12 col-lg-7 col-lg-push-5" >
               <div className="card mx-auto mt-5 shadow-lg p-3 mb-5 bg-white rounded" style={{maxWidth:"90%",width:"800px"}}>
                 <div className="card-body">
-                    <h3 className="text-center">Transaction Details</h3>
+                    <h3 className="text-center">Transaction Details {showMonster()}</h3>
                     <div style={{ width: "70%" }} className="mx-auto mt-5">
                       {showForm()}
-                    
                     </div>
                 </div>
               </div>
+              </div>
+              <div className="col-md-12 col-lg-5  col-lg-pull-7 my-auto monster-section " >
+                <h4 className="text-center">Choose a monster</h4>
+                <Monsters
+                  monsters={monsters}
+                  selectMonster={selectMonster}
+                />
+                <br/>
+              </div>
         </div>
     </div>
-      </div>
+      // </div>
   );
 };
 
