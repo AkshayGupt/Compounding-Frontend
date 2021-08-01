3643c741197aeab6b467c266288891eee9511b96
AkshayGupt <189301019.akshay@gmail.com>
2021-08-01 12:45:49 +0530

    :art: Restyling Landing page"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import Slides from './components/Introduction/Slides';
-import Landing from './components/Landing/Landing'
+import LandingScreen from './components/Landing/LandingScreen';
+import Home from './components/Home/Home'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Transactions from './components/Transactions/Transactions';
 import TransactionView from './components/Transactions/TransactionView';
 
 function App() {
   return (
-
     <div className="App">
        <Router>
         <Switch>
-          {/* Public Routes */}
-          <Route path="/" exact component={Slides} />
-          <Route path="/landing" exact component={Landing} />
+          <Route path="/" exact component={LandingScreen} />
+          <Route path="/home" exact component={Home} />
           <Route path="/transactions" exact component={Transactions} />
           <Route path="/transaction/:id" exact component={TransactionView} />
         </Switch>
