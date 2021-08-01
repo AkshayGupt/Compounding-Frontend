cfab75ad6f9bd968c6bc4bddf54889e6f41e8783
AkshayGupt <189301019.akshay@gmail.com>
2021-08-01 12:49:03 +0530

    :truck: Including new files

---
+++ src/components/Landing/Slides.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import './styles.css';
+
+const Slides = ({
+    img1,
+    img2,
+    img3,
+    icon1,
+    icon2,
+    icon3
+}) => {
+    return (
+        <div style={{width:"100%"}}>
+        <Carousel autoPlay showThumbs={false} width="50vw" infiniteLoop showArrows={false} showStatus={false}>
+            <div>
+                <img src={img1.src} className="image-slide "/>
+                <p className="pt-3 text-white text">{img1.text}</p>
+                <p className="pb-1 text-center" >{icon1}</p>
+            </div>
+            <div>
+                <img src={img2.src} className="image-slide " />
+                <p className="pt-3 text-white text">{img2.text}</p>
+                <p className="pb-1 text-center" >{icon2}</p>
+            </div>
+            <div>
+                <img src={img3.src} className="image-slide " />
+                <p className="pt-3 text-white ">{img3.text}</p>
+                <p className="pb-4  text-center " >{icon3}</p>
+            </div>
+        </Carousel>
+        </div>
+    )
+}
+
+export default Slides;
---
+++ src/components/Transactions/Monsters.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Monsters() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Monsters
