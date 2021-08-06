49b68b4fcc53a55fc94108071e07b4a5dbfb9ca3
Rakesh Sagitra <rakesh.sagitra@openpayd.com>
2021-08-06 22:54:51 +0530

    reward screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import LandingScreen from './components/Landing/LandingScreen';
-import Home from './components/Home/Home'
+import LandingScreen from "./components/Landing/LandingScreen";
+import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Transactions from './components/Transactions/Transactions';
-import TransactionView from './components/Transactions/TransactionView';
-import QuestionsBot from './components/Details/QuestionsBot';
-import QuestDetails from './components/Quests/GroupQuests/QuestDetails';
+import Transactions from "./components/Transactions/Transactions";
+import TransactionView from "./components/Transactions/TransactionView";
+import QuestionsBot from "./components/Details/QuestionsBot";
+import QuestDetails from "./components/Quests/GroupQuests/QuestDetails";
+import RewardsTabs from "./components/club/rewards";
 
 function App() {
   return (
     <div className="App">
-       <Router>
+      <Router>
         <Switch>
           <Route path="/" exact component={LandingScreen} />
           <Route path="/home" exact component={Home} />
@@ -17,6 +18,7 @@ function App() {
           <Route path="/transaction/:id" exact component={TransactionView} />
           <Route path="/details" exact component={QuestionsBot} />
           <Route path="/groupQuest" exact component={QuestDetails} />
+          <Route path="/rewards" exact component={RewardsTabs} />
         </Switch>
       </Router>
     </div>
